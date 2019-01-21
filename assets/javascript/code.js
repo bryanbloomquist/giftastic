594ade766caaa3e6d8b248434eae31a99d31fc0c
bloomy215 <bmbloomquist@gmail.com>
2019-01-20 23:41:19 -0600

    coding finished

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,4 +1,4 @@
-var topics = ["Brock Sampson","Henchman 21","The Monarch","Sterling Archer","Pam Poovey","Cheryl Tunt","Bugs Bunny","Roger Rabbit","Stewie Griffin","Homer Simpson","Twilight Sparkle","Rainbow Dash","Pinkie Pie","Pinky and the Brain"];
+var topics = ["Star Wars Robot Chicken","Adventure Time","The Venture Bros.","Aqua Teen Hunger Force","Robot Chicken","Rick and Morty","Archer","South Park","Futurama","The Simpsons"];
 
 var searchNum = "10";
 
@@ -11,25 +11,46 @@ function displayGIF() {
     }).then(function(response) {
         console.log(response);
         var results = response.data;
-        console.log(results);
-        debugger;
         for (var i = 0; i < results.length; i++){
-            var cartoonDiv = $("<div>");
-            var ratingElement = $("<p>").text(results[i].rating);
-            var cartoonGIF = $("<img>").attr("src", results[i].images.fixed_width_still.url)
+            var cartoonDiv = $("<div class='gif-box border border-info bg-warning p-1 m-1 d-block float-left'>");
+            var ratingElement = $("<p>").text('Rating: '+results[i].rating);
+            var cartoonGIF = $("<img>")
+                .addClass("gif")
+                .attr("src", results[i].images.fixed_width_still.url)
                 .attr("data-still", results[i].images.fixed_width_still.url)
                 .attr("data-animate", results[i].images.fixed_width.url);
-            cartoonDiv.append(ratingElement, cartoonGIF);
+            cartoonDiv.append(cartoonGIF, ratingElement);
             $("#gifZone").prepend(cartoonDiv);
         }
     });
 }
 
+$(document).on("click", ".gif", function(){
+    var clickedElement = $(this);
+    var state = clickedElement.attr("data-state");
+    var clickedElement = $(this);
+    var state = clickedElement.attr("data-state");
+    if(state === 'still') {
+        var animateSrc = clickedElement.attr("data-animate");
+        clickedElement.attr("src", animateSrc);
+        clickedElement.attr("data-state", "animate");
+    } else {
+        var stillSrc = clickedElement.attr("data-still");
+        clickedElement.attr("src", stillSrc);
+        clickedElement.attr("data-state", "still");
+    }
+});
+
+$("#change-number").on("click", function(event){
+    event.preventDefault();
+    searchNum = $("#gif-num").val();
+});
+
 function renderButtons() {
     $("#buttonZone").empty();
     for(var i=0; i<topics.length; i++) {
         var b = $("<button>");
-        b.addClass("cartoon");
+        b.addClass("cartoon m-1");
         b.attr("data-name", topics[i]);
         b.text(topics[i]);
         $("#buttonZone").append(b);
